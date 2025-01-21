647cc975f92e61bfe670240d9ec8687cef797a81
nareshr21 <nareshr01030@gmail.com>
2025-01-22 03:13:54 +0530

    Create linear_search.py
    Search an element using linear search

---
+++ linear_search.py
@@ -0,0 +1,16 @@
+def linear_search_program(arr, target):
+    "Perform a linear search to find the target in the array."
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Return the index where the target is found
+    return -1  # Return -1 if the target is not found
+
+def main():
+    arr = [10, 20, 30, 40, 50]
+    target = int(input("Enter the number to search: "))
+    result = linear_search_program(arr, target)
+    if result != -1:
+        print(f"Number found at index {result}.")
+    else:
+        print("Number not found.")
+
