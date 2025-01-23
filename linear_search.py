bc45cc45c6b7f4268114722cea88913ae92a15fd
nareshr21 <nareshr01030@gmail.com>
2025-01-23 17:15:17 +0530

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,15 +1,22 @@
-def linear_search_program(arr, target):
-    "Perform a linear search to find the target in the array."
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i  # Return the index where the target is found
-    return -1  # Return -1 if the target is not found
+def find_target_element_index(arr,target):
+    
+    #Finding the target element index in  an array by taking input from user 
 
-arr = [10, 20, 30, 40, 50]
-target = int(input("Enter the number to search: "))
-result = linear_search_program(arr, target)
-if result != -1:
-    print(f"Number found at index {result}.")
+
+    n=len(arr)
+    
+    for i in range(n):
+        if arr[i]==target: #Returning the index of target element
+            return i
+
+    return -1 #Returning -1 if target element is not found
+
+arr=[10, 20, 30, 40, 50]
+target=int(input("Enter the target element:"))
+
+
+result=find_target_element_index(arr,target)
+if result!=-1:
+    print("Element found at index:",result)
 else:
-    print("Number not found.")
-
+    print("Element is not found")
